54cd7f648542fc6ff1c80dc613e2ed86e7f6ef5d
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-07-04 12:57:27 +0200

    feat(data): use prisma client for queries Fixes #19

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
